2a5766b95d8ce4c92e313e554c6f026c98613ba4
Carolina Josephik <carolina.moraesjosephik@gmail.com>
2023-07-09 16:07:49 -0300

    chore: create database config

---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+
+@Injectable()
+export class TypeOrmConfigService implements TypeOrmOptionsFactory {
+  createTypeOrmOptions(): TypeOrmModuleOptions {
+    return {
+      type: 'postgres',
+      database: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      synchronize: true,
+      entities: ['dist/**/*.entity{.ts,.js}'],
+    };
+  }
+}
