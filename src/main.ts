e45296181de24dcebef4f611ee67bd0bfb25584f
Carolina de Moraes Josephik <carolina.moraesjosephik@gmail.com>
2024-12-16 12:22:18 -0300

    feat: enable cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({ origin: process.env.APP_URL, credentials: true });
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new HttpExceptionFilter());
   app.use(cookieParser());
