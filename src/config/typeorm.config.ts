06d755694496b0b353426d7ac60e6e18b3db9e25
Carolina de Moraes Josephik <carolina.moraesjosephik@gmail.com>
2025-01-27 22:04:49 -0300

    chore: improve CI config and create some debugging

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -4,6 +4,13 @@ import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
 @Injectable()
 export class TypeOrmConfigService implements TypeOrmOptionsFactory {
   createTypeOrmOptions(): TypeOrmModuleOptions {
+    console.log('>>>>>>>>>>>>>>>>');
+    console.log('name: ' + process.env.DB_NAME);
+    console.log('host: ' + process.env.DB_HOST);
+    console.log('port: ' + process.env.DB_PORT);
+    console.log('username: ' + process.env.DB_USERNAME);
+    console.log('password: ' + process.env.DB_PASSWORD);
+
     return {
       type: 'postgres',
       database: process.env.DB_NAME,
