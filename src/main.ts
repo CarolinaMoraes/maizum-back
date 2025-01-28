c7a0720a3cfcf2670d5e99836964d7f99417d4ca
Carolina de Moraes Josephik <carolina.moraesjosephik@gmail.com>
2025-01-27 22:19:41 -0300

    fix: add dotenv

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter } from './common/filters/httpException.filter';
 import * as cookieParser from 'cookie-parser';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
